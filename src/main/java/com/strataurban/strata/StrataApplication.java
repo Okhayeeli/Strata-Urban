92af8874e2c167c4e7571c75020f66aed9b7599a
Wisdom Donald-Ase Nengi <76922069+Wiziwax@users.noreply.github.com>
2026-01-10 21:22:14 +0100

    Work on the Verification Stuff

--- src/main/java/com/strataurban/strata/StrataApplication.java
+++ src/main/java/com/strataurban/strata/StrataApplication.java
@@ -1,6 +1,5 @@
 package com.strataurban.strata;
 
-import com.strataurban.strata.Security.SecurityUserDetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
